86e6bf49160b75ef8807b70c768da84b032eace5
oderindeolayemi <oderinde.yemi@gmail.com>
2019-12-10 03:07:09 +0100

    Updates

---
+++ precache-manifest.95ef59daa6c071748b61843558af75ac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ecb69d70e308ae71867d835069c5b92",
+    "url": "/eazyfood/index.html"
+  },
+  {
+    "revision": "4484840c2f198313941e",
+    "url": "/eazyfood/static/css/main.10d2ea02.chunk.css"
+  },
+  {
+    "revision": "8dd5815649ba3b479062",
+    "url": "/eazyfood/static/js/2.0ca6eeba.chunk.js"
+  },
+  {
+    "revision": "4484840c2f198313941e",
+    "url": "/eazyfood/static/js/main.4cd60fac.chunk.js"
+  },
+  {
+    "revision": "2806b60418719d8393cb",
+    "url": "/eazyfood/static/js/runtime-main.5fe0f997.js"
+  },
+  {
+    "revision": "5ed76bf52743ef5d29a008e4e0e70e57",
+    "url": "/eazyfood/static/media/font.5ed76bf5.svg"
+  },
+  {
+    "revision": "b2e7684fb2b1805d143b27b831a76ae5",
+    "url": "/eazyfood/static/media/hash-key.b2e7684f.svg"
+  },
+  {
+    "revision": "a3a5e86b62dce1b5046358fc2f456626",
+    "url": "/eazyfood/static/media/home.a3a5e86b.svg"
+  },
+  {
+    "revision": "abd7d65460aa36042c003459dc5ed661",
+    "url": "/eazyfood/static/media/programming.abd7d654.png"
+  },
+  {
+    "revision": "b547fa37faba438ff773f492dd47f212",
+    "url": "/eazyfood/static/media/puzzle.b547fa37.png"
+  }
+]);
